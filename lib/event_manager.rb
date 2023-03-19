7fb7c8e8d0d6b1e3a8bfb217207a40ce6c278c72
Joe W <jweber4213@gmail.com>
2023-03-19 19:05:09 +0900

    created erb form letter, added phone# check

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,8 +1,26 @@
+require 'date'
 require 'csv'
 require 'google/apis/civicinfo_v2'
+require 'erb'
 
 def clean_zipcode(zipcode)
   zipcode.to_s.rjust(5, '0')[0..4]
+end
+
+def clean_phone_number(phone_number)
+    check = phone_number.to_s.gsub(/\D/, '')
+    case check.chars.count
+    when 10
+        check
+    when 11
+        if check[0] == 1
+            return check.slice(1..10)
+        else
+            return " "
+        end
+    else
+        return " "
+    end
 end
 
 def legislators_by_zipcode(zip)
@@ -10,41 +28,52 @@ def legislators_by_zipcode(zip)
     civic_info.key = 'AIzaSyBvvZwQbA5IV6_QbobgQ6A_ZJlt5B7GiWg'
 
     begin
-        legislators = civic_info.representative_info_by_address(
+        civic_info.representative_info_by_address(
             address: zip,
             levels: 'country',
             roles: ['legislatorUpperBody', 'legislatorLowerBody']
-          )
-          legislators = legislators.officials
-          legislator_names = legislators.map(&:name)
-          legislator_names.join(", ")
+          ).officials
     rescue
         'You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials'
     end
 
 end
 
+def save_thank_you_letter(id, form_letter)
+    #directory named “output” if a directory named “output” does not already exist.
+    Dir.mkdir('output') unless Dir.exist?('output') 
+
+    filename = "output/thanks_#{id}.html"
+
+    File.open(filename, 'w') do |file| # w - want to open the file for writing
+        file.puts form_letter #file inherits from IO#puts acts like Kernel#puts
+    end
+end
+
 puts 'EventManager initialized.'
 
-template_letter = File.read('form_letter.html')
-
 contents = CSV.open(
-  'event_attendees.csv',
-  headers: true,
-  header_converters: :symbol
+    'event_attendees.csv',
+    headers: true,
+    header_converters: :symbol
 )
 
+template_letter = File.read('form_letter.erb')
+erb_template = ERB.new template_letter
+
 contents.each do |row|
-  name = row[:first_name]
+    id = row[0]
+    name = row[:first_name]
 
-  zipcode = clean_zipcode(row[:zipcode])
+    zipcode = clean_zipcode(row[:zipcode])
 
-  legislators = legislators_by_zipcode(zipcode)
+    phone = clean_phone_number(row[:homephone])
 
-  personal_letter = template_letter.gsub('FIRST_NAME', name)
-  #personal_letter = personal_letter.gsub('LEGISLATORS', legislators)
-  personal_letter.gsub!('LEGISLATORS', legislators)
+    legislators = legislators_by_zipcode(zipcode)
 
-  #puts "#{name} #{zipcode} #{legislators}"
-  puts personal_letter
+    #instance of binding knows all about the current state of variables and methods within the given scope. 
+    form_letter = erb_template.result(binding) 
+
+    #puts "#{name} #{phone}"
+    save_thank_you_letter(id, form_letter)
 end
