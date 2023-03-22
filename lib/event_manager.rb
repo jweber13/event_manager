22455d73d0805118cb6157054ff595288cedb43d
Joe W <jweber4213@gmail.com>
2023-03-22 22:44:22 +0900

    added date / time management, calucate peak registration hours and record days

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,4 +1,5 @@
 require 'date'
+require 'time'
 require 'csv'
 require 'google/apis/civicinfo_v2'
 require 'erb'
@@ -61,6 +62,52 @@ contents = CSV.open(
 template_letter = File.read('form_letter.erb')
 erb_template = ERB.new template_letter
 
+class EventDateTime
+    attr_accessor :times
+    attr_accessor :days
+
+    def initialize
+        @times = []
+        @days = []
+    end
+
+    def clean_date_time(d_t)
+        puts d_t
+        @times << Time.strptime(d_t, "%m/%d/%y %H:%M").hour
+        #the date_time argument is in the format 11/12/08 10:47, which includes a date component. To parse a date and time string in the format 11/12/08 10:47, you should use the %m/%d/%y %H:%M format string. For example:
+        @days << Time.strptime(d_t, '%m/%d/%y %H:%M').wday
+    end
+
+    public
+    def get_peak_times
+        hours = @times.sort
+        max_consecutive_hours = []
+        current_consecutive_hours = []
+
+        hours.each_with_index do |h, i|
+            #checks if the current hour h is one more than the previous hour in the hours list.
+            #h == hours[i-1]+1 evaluates to 6 == hours[3]+1
+            if i > 0 && h == hours[i-1]+1
+                current_consecutive_hours << h
+            else
+                # If this hour is not consecutive with the previous hour, check if the current block is bigger than the previous maximum
+                if current_consecutive_hours.length > max_consecutive_hours.length
+                    max_consecutive_hours = current_consecutive_hours
+                end
+                #start a new block with this hour
+                current_consecutive_hours[h]
+            end
+        end
+        # If the current block is bigger than the previous maximum, update the maximum
+        if current_consecutive_hours.length > max_consecutive_hours.length
+            max_consecutive_hours = current_consecutive_hours
+        end
+
+        return max_consecutive_hours.join(", ")
+    end
+end
+
+date_time = EventDateTime.new
 contents.each do |row|
     id = row[0]
     name = row[:first_name]
@@ -69,11 +116,17 @@ contents.each do |row|
 
     phone = clean_phone_number(row[:homephone])
 
+    date_time.clean_date_time(row[:regdate])
+
     legislators = legislators_by_zipcode(zipcode)
 
     #instance of binding knows all about the current state of variables and methods within the given scope. 
     form_letter = erb_template.result(binding) 
 
-    #puts "#{name} #{phone}"
-    save_thank_you_letter(id, form_letter)
+    puts "#{name} #{phone}"
+    #save_thank_you_letter(id, form_letter)
 end
+
+puts "The peak registration hours are: #{date_time.get_peak_times}"
+puts "The peak registration days are: #{date_time.days}"
+
